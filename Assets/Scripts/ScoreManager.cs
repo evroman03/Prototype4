00b069d1e28cf21427404c12fe3d4299b0770119
Veizyl <sturner@mail.bradley.edu>
2024-04-15 16:33:16 -0500

    Update ScoreManager.cs

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -3,37 +3,47 @@ using TMPro;
 
 public class ScoreManager : MonoBehaviour
 {
-    #region Singleton
-    private static ScoreManager instance;
-    public static ScoreManager Instance
-    {
-        get
-        {
-            if (instance == null)
-                instance = FindAnyObjectByType(typeof(ScoreManager)) as ScoreManager;
-            return instance;
-        }
-        set
-        {
-            instance = value;
-        }
-    }
-    #endregion
     public TMP_Text scoreText; // Reference to the TextMeshPro Text component where the score will be displayed
     public TMP_Text multiplierText; //Reference to the TextMeshPro Text component where the multiplier will be displayed
     private int score = 0; // The current score
     private int multiplier = 1; // The current multiplier
+    private float timeSinceHit;
+    [SerializeField] int levelsToRemove = 1;
 
     private void Start()
     {
         UpdateScoreText();
         UpdateMultiplierText();
     }
+    private void Update()
+    {
+        if(timeSinceHit >= 15 || timeSinceHit < 30)
+        {
+            multiplier = 2;
+        }
+        else if(timeSinceHit >= 30 || timeSinceHit < 45)
+        {
+            multiplier = 3;
+        }
+        else if(timeSinceHit >= 45 || timeSinceHit < 60)
+        {
+            multiplier = 4;
+        }
+    }
     // Function to add points to the score
     public void ChangeScore(int points)
     {
         score += points * multiplier;
         UpdateScoreText();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        multiplier += -levelsToRemove;
+        if (multiplier <= 0)
+        {
+            multiplier = 1;
+        }
     }
 
     // Function to update the score text in the UI
