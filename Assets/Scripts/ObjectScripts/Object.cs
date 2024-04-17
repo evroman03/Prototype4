42502abbab599886d1f7464a61713519af9d628c
evroman03 <evroman@mail.bradley.edu>
2024-04-17 13:58:08 -0500

    BarrelsMoveYay

--- Assets/Scripts/ObjectScripts/Object.cs
+++ Assets/Scripts/ObjectScripts/Object.cs
@@ -1,11 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Object : MonoBehaviour
 {
     public int ScoreChangeFactor;
     public float EnemyCarDistanceChange;
+    public bool isBarrel;
+    private Rigidbody rb;
+    private GameObject moveTowards;
+    private EnemyCarController enemyCar;
+
+
+    private void Start()
+    {
+        if (isBarrel)
+        {
+            enemyCar = GameController.Instance.Enemy.GetComponentInChildren<EnemyCarController>();
+            rb = GetComponent<Rigidbody>();
+            for(int i =0; i < LaneManager.Instance.LaneEnds.Length; i++)
+            {
+                if(enemyCar.currentSnap == i)
+                {
+                    moveTowards = LaneManager.Instance.LaneEnds[i];
+                }
+            }
+        }
+
+    }
+    private void Update()
+    {
+        if (isBarrel)
+        {
+            var step = (GameController.Instance.BackgroundSpeed/2) * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(rb.position, moveTowards.transform.position, step);
+        } 
+    }
 
     private void OnTriggerEnter(Collider other)
     {
